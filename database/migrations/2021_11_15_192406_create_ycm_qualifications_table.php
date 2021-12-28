4d99e462a54a3ccd4ffe4802de8c8bec6e2511c7
tshoaib6 <43224905+tshoaib6@users.noreply.github.com>
2021-12-28 13:56:51 +0500

    latest

---
+++ database/migrations/2021_11_15_192406_create_ycm_qualifications_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateYcmQualificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ycm_qualifications', function (Blueprint $table) {
+            $table->id();
+            $table->string('ycm_id');
+            $table->foreign('ycm_id')->references('rec_id')->on('Employees');
+            $table->string('Insitute');
+            $table->string('course');
+            $table->int('year',4);
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ycm_qualifications');
+    }
+}
--- resources/views/form/employeelist.blade.php
+++ resources/views/form/employeelist.blade.php
@@ -155,30 +155,6 @@
     </div>
     <!-- /Sidebar -->
 
-    <!-- Page Wrapper -->
-    <div class="page-wrapper">
-        <!-- Page Content -->
-        <div class="content container-fluid">
-            <!-- Page Header -->
-            <div class="page-header">
-                <div class="row align-items-center">
-                    <div class="col">
-                        <h3 class="page-title">Employee</h3>
-                        <ul class="breadcrumb">
-                            <li class="breadcrumb-item"><a href="index.html">Dashboard</a></li>
-                            <li class="breadcrumb-item active">Employee</li>
-                        </ul>
-                    </div>
-                    <div class="col-auto float-right ml-auto">
-                        <a href="#" class="btn add-btn" data-toggle="modal" data-target="#add_employee"><i class="fa fa-plus"></i> Add Employee</a>
-                        <div class="view-icons">
-                            <a href="{{ route('all/employee/card') }}" class="grid-view btn btn-link active"><i class="fa fa-th"></i></a>
-                            <a href="{{ route('all/employee/list') }}" class="list-view btn btn-link"><i class="fa fa-bars"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-			<!-- /Page Header -->
 
             <!-- Search Filter -->
             <form action="{{ route('all/employee/list/search') }}" method="POST">
